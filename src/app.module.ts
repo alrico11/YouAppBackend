16b245b9124e2ad6a5ecdcb9ac7e3e3153ced538
alricowibowo <alricowibowo@gmail.com>
2024-02-18 19:44:04 +0700

    feat: get,update,create profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { MessageController } from './message/message.controller';
 import * as dotenv from 'dotenv';
 dotenv.config()
 @Module({
@@ -12,5 +14,7 @@ dotenv.config()
     AuthModule,
     ProfileModule,
   ],
+  providers: [RabbitmqService],
+  controllers: [MessageController],
 })
 export class AppModule {}
